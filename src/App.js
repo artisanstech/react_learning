05be7462fff1a1d3718c79d8675798fd430de0f9
lucifergeeks <lucifergeeks@gmail.com>
2022-04-06 22:35:59 +0530

    Inital commit 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/components/Counter.js 	new file:   src/components/Greet.js 	new file:   src/components/Hello.js     new file:   src/components/Message.js 	new file:   src/components/Welcome.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Hello from "./components/Hello";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import Message from "./components/Message";
+import "./App.css";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
+      {/* <Message /> */}
+      {/* <Greet name="Bruce" heroName="Batman">
+        <p>This is Childer props</p>
+      </Greet>
+      <Greet name="Clark" heroName="Superman">
+        <button>Action</button>
+      </Greet>
+      <Greet name="Diana" heroName="Spiderman" />
+      <Welcome name="Bruce" heroName="Batman"></Welcome>
+      <Welcome name="Clark" heroName="Superman"></Welcome>
+      <Welcome name="Diana" heroName="Spiderman"></Welcome> */}
     </div>
   );
 }
